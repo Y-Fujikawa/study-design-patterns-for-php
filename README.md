497e7fd59854a6274928a2defec79fbe0aec4f70
Y-Fujikawa <fujikawa0@gmail.com>
2021-10-01 15:51:34 +0900

    README.mdに手順追加、トップページにリンクだけ作成

--- README.md
+++ README.md
@@ -4,6 +4,26 @@
 
 [PHPによるデザインパターン入門](https://shimooka.hateblo.jp/entry/20141211/1418298136) を元にデザインパターンを勉強するリポジトリ
 
+## インストール
+
+```shell
+git clone git@github.com:Y-Fujikawa/study-design-patterns-for-php.git
+rm -fr ./src/*
+docker-compose build
+docker-compose up
+```
+
 ## やり方
 
 概要にあるページを確認して、デザインパターンごとにディレクトリを作成して写経する。
+
+例： `TemplateMethod` の場合
+
+```shell
+mkdir TemplateMethod
+cd TemplateMethod
+```
+
+各デザインパターンごとのディレクトリ配下で写経する。
+
+`http://localhost/` にアクセスして各デザインパターンの結果を確認する。
--- src/index.php
+++ src/index.php
@@ -13,8 +13,42 @@
     <!-- メインコンテンツ -->
     <main>
         <ul>
-            <li><a href="./TemplateMethod/template_method_client.php" target="_blank" rel="noopener noreferrer">Template Method</a></li>
-            <li><a href="./Singleton/singleton_client.class.php" target="_blank" rel="noopener noreferrer">Singleton</a></li>
+            <li>STEP1：まずはここから</li>
+            <ul>
+                <li><a href="./TemplateMethod/template_method_client.php" target="_blank" rel="noopener noreferrer">Template Method 〜処理を穴埋めする</a></li>
+                <li><a href="./Singleton/singleton_client.class.php" target="_blank" rel="noopener noreferrer">Singleton 〜いくつ作るかを制限する</a></li>
+                <li><a href="./Adapter/adapter_client.php" target="_blank" rel="noopener noreferrer">Adapter 〜APIを変更する</a></li>
+            </ul>
+        </ul>
+        <ul>
+            <li>STEP2：少し慣れたら</li>
+            <ul>
+                <li><a href="./FactoryMethod/factory_client.php" target="_blank" rel="noopener noreferrer">FactoryMethod 〜生成処理と使用処理を分離する</a></li>
+                <li><a href="./Façade/facade_client.php" target="_blank" rel="noopener noreferrer">Façade 〜シンプルな唯一の窓口</a></li>
+                <li><a href="./Iterator/iterator_client.php" target="_blank" rel="noopener noreferrer">Iterator 〜順々にアクセスする</a></li>
+            </ul>
+        </ul>
+        <ul>
+            <li>STEP3：どんどん使ってみよう</li>
+            <ul>
+                <li><a href="./AbstractFactory/abstract_factory_client.php" target="_blank" rel="noopener noreferrer">AbstractFactory 〜関連する部品をまとめて作る工場</a></li>
+                <li><a href="./Bridge/xxx.php" target="_blank" rel="noopener noreferrer">Bridge 〜実装と機能の架け橋</a></li>
+                <li><a href="./Builder/xxx.php" target="_blank" rel="noopener noreferrer">Builder 〜生成の手順と手段を分離する</a></li>
+                <li><a href="./ChainofResponsibility/xxx.php" target="_blank" rel="noopener noreferrer">Chain of Responsibility 〜処理のたらい回し</a></li>
+                <li><a href="./Command/xxx.php" target="_blank" rel="noopener noreferrer">Command 〜要求をクラスで表す</a></li>
+                <li><a href="./Composite/xxx.php" target="_blank" rel="noopener noreferrer">Composite 〜木構造を表す</a></li>
+                <li><a href="./Decorator/xxx.php" target="_blank" rel="noopener noreferrer">Decorator 〜かぶせて機能UP</a></li>
+                <li><a href="./Flyweight/xxx.php" target="_blank" rel="noopener noreferrer">Flyweight 〜同じものは一度しか作らない</a></li>
+                <li><a href="./Interpreter/xxx.php" target="_blank" rel="noopener noreferrer">Interpreter 〜言語の文法表現を通訳する</a></li>
+                <li><a href="./Mediator/xxx.php" target="_blank" rel="noopener noreferrer">Mediator 〜すべては相談役が知っている</a></li>
+                <li><a href="./Memento/xxx.php" target="_blank" rel="noopener noreferrer">Memento 〜スナップショットを取る</a></li>
+                <li><a href="./Observer/xxx.php" target="_blank" rel="noopener noreferrer">Observer 〜状態変化を通知する</a></li>
+                <li><a href="./Prototype/xxx.php" target="_blank" rel="noopener noreferrer">Prototype 〜コピーして作る</a></li>
+                <li><a href="./Proxy/xxx.php" target="_blank" rel="noopener noreferrer">Proxy 〜具体的な実装を隠す身代わり</a></li>
+                <li><a href="./State/xxx.php" target="_blank" rel="noopener noreferrer">State 〜状態を表す</a></li>
+                <li><a href="./Strategy/xxx.php" target="_blank" rel="noopener noreferrer">Strategy 〜戦略を切り替える</a></li>
+                <li><a href="./Visitor/xxx.php" target="_blank" rel="noopener noreferrer">Visitor 〜要素と要素に対する操作を分離する</a></li>
+            </ul>
         </ul>
     </main>
 </body>
