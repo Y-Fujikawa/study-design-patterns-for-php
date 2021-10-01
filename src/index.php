a9bdd9474fdfd920078ccaffd9375515ffcef7e4
Y-Fujikawa <fujikawa0@gmail.com>
2021-10-01 12:20:18 +0900

    試しにデザインパターン2つを実装して結果を表示させてみた

--- src/index.php
+++ src/index.php
@@ -1,2 +1,21 @@
-<?php
-    echo phpinfo();
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ページタイトル</title>
+</head>
+<body>
+    <!-- ヘッダー -->
+    <header>
+        デザインパターン
+    </header>
+ 
+    <!-- メインコンテンツ -->
+    <main>
+        <ul>
+            <li><a href="./TemplateMethod/template_method_client.php" target="_blank" rel="noopener noreferrer">Template Method</a></li>
+            <li><a href="./Singleton/singleton_client.class.php" target="_blank" rel="noopener noreferrer">Singleton</a></li>
+        </ul>
+    </main>
+</body>
+</html>
